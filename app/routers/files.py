c784cfd6d044ebcf47f58317c6792597e069938a
nhan <nhoang2929@gmail.com>
2025-05-17 22:51:49 +0700

    add: download file api from s3

--- app/routers/files.py
+++ app/routers/files.py
@@ -8,3 +8,7 @@ router = APIRouter(tags=['files'])
 @router.get('/files')
 async def get_list_file(conn : Connection = Depends(get_db_pool), limit : int = 10, page : int = 1): 
     return await save_file_services.get_list_pdf_uploaded(conn, limit, (page - 1) * limit)
+
+@router.post('/download/{fileName}')
+async def download_file(fileName : str):
+    return save_file_services.download_file(fileName)
